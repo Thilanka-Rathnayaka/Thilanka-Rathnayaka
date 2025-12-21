4bff0807fe4cff4524c27bf4205537c997ff9a3f
Thilanka Yasodhana Rathnayaka <thilankayasodhana@gmail.com>
2025-12-21 20:56:27 +0530

    Update script.js
    Exp update

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ window.addEventListener('scroll', () => {
 
 // Stats animation
 const stats = document.querySelectorAll('.stat-number');
-const statValues = [7, 100, 15];
+const statValues = [8, 100, 15];
 
 if (stats.length) {
     const observer = new IntersectionObserver((entries) => {
